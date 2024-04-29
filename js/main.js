36d0d5c7296cd0256f3c0767c106b4feb6a890a2
LuckyLoo <101459565+LuckyLoo1730@users.noreply.github.com>
2024-04-29 22:35:31 +0100

    Updated isMobileUser

--- js/main.js
+++ js/main.js
@@ -10,12 +10,16 @@ updateTime();
 setInterval(updateTime, 1000)
 function isMobileDevice() {
     var mobileDeviceRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-      return mobileDeviceRegex.test(navigator.userAgent);
+    return mobileDeviceRegex.test(navigator.userAgent);
   }
-    function redirectMobileUsers() {
-    var redirectUrl = "https://google.com";
-      window.location.href = redirectUrl;
+  
+  function displayMobileMessage() {
+    var message = "Please use a laptop to access this website.";
+    alert(message);
+    var redirectUrl = "https://smilenodes.xyz";
+    window.location.href = redirectUrl;
   }
+  
   if (isMobileDevice()) {
-    redirectMobileUsers();
+    displayMobileMessage();
   }
